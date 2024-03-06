a222b80a6b3b6a4797d5cc99550f420edad1af8a
Yigit Yuce <ygtyce@gmail.com>
2024-03-06 22:07:28 +0300

    provides better example for the failing case

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,11 +1,4 @@
-import { Component, ComponentInterface, Host, State, h } from '@stencil/core';
-
-const DEFAULT_ITEM_COUNT = 8;
-const getRandomInt = (min: number, max: number): number => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+import { Component, ComponentInterface, Host, h } from '@stencil/core';
 
 @Component({
   tag: 'my-component',
@@ -14,49 +7,13 @@ const getRandomInt = (min: number, max: number): number => {
   scoped: true,
 })
 export class MyComponent implements ComponentInterface {
-  private nestedTestExampleRef: HTMLMyNestedComponentElement;
-  @State() dynamicTestItems: HTMLElement[] = [];
-
-  componentWillLoad(): void | Promise<void> {
-    this.dynamicTestItems = this.createTestVNodes();
-  }
-
-  private createTestVNodes = (count = DEFAULT_ITEM_COUNT, shuffle = false): HTMLElement[] => {
-    const items = Array.from(new Array(count)).map((_, i) => <span class="default-slot-item">{`item-${i}`}</span>);
-    return shuffle ? items.sort(() => Math.random() - 0.5) : items;
-  };
-
-  private createTestElements = (count = DEFAULT_ITEM_COUNT, shuffle = false): HTMLElement[] => {
-    const items = Array.from(new Array(count)).map((_, i) => {
-      const element = document.createElement('span');
-      element.className = 'default-slot-item';
-      element.textContent = `item-${i}`;
-      return element;
-    });
-    return shuffle ? items.sort(() => Math.random() - 0.5) : items;
-  };
-
   render() {
     return (
       <Host>
-        <my-nested-component ref={el => (this.nestedTestExampleRef = el)}>
+        <my-nested-component>
           <span slot="header">Header Text</span>
-          {this.dynamicTestItems}
+          <slot />
         </my-nested-component>
-
-        <button
-          onClick={() => {
-            const items = Array.from(this.nestedTestExampleRef?.querySelectorAll('.default-slot-item') || []);
-            for (let i = 0; i < items.length; i++) {
-              items[i].remove();
-            }
-            for (const item of this.createTestElements(getRandomInt(1, 20))) {
-              this.nestedTestExampleRef?.appendChild(item);
-            }
-          }}
-        >
-          Randomize Item Count
-        </button>
       </Host>
     );
   }
